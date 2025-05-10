5f089faacca5bfea68a999c81a7a8b1f76f2880c
Romina-Piazza <piazzaromina15m@gmail.com>
2025-05-10 17:23:16 +0200

    prima bozza

---
+++ progetti Front-End romy/portfolio.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // animazione barre competenze
+  const skillLevels = document.querySelectorAll('.skill-level');
+  skillLevels.forEach(bar => {
+    const level = bar.dataset.level;
+    bar.style.width = level;
+  });
+
+  // particelle
+  particlesJS('particles-js', {
+    particles: {
+      number: { value: 80, density: { enable: true, value_area: 800 } },
+      color: { value: "#00BFFF" },
+      shape: { type: "circle" },
+      opacity: {
+        value: 0.5,
+        random: true,
+        anim: { enable: true, speed: 1, opacity_min: 0.1 }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: true, speed: 40, size_min: 0.1 }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        random: true,
+        out_mode: "out"
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" },
+        resize: true
+      }
+    },
+    retina_detect: true
+  });
+});
